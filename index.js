61d50315684890c9c0c480438fcf509fb6d3fac7
Dan Clair <danclair@me.com>
2022-08-22 15:47:26 -0500

    made changes to order functions

--- index.js
+++ index.js
@@ -5,41 +5,89 @@ import chalk from 'chalk';
 
 const prompt = promptSync();
 
-const Breakfast = {
-    0: "Water",
-    1: "Eggs",
-    2: "Toast",
-    3: "Coffee"
+const Meal = {
+    "Breakfast": {
+        0: "Water",
+        1: "Eggs",
+        2: "Toast",
+        3: "Coffee"
+    },
+    "Lunch": {
+        0: "Water",
+        1: "Sandwich",
+        2: "Chips",
+        3: "Soda"
+    },
+    "Dinner": {
+        0: "Water",
+        1: "Steak",
+        2: "Potatoes",
+        3: "Wine",
+        4: "Cake"
+    }
+}
 
-}
-const Lunch = {
-    0: "Water",
-    1: "Sandwich",
-    2: "Chips",
-    3: "Soda"
-}
-const Dinner = {
-    0: "Water",
-    1: "Steak",
-    2: "Potatoes",
-    3: "Wine",
-    4: "Cake"
-}
 
 
 
 class Menu {
     constructor() {
         this.order = [];
-    }
+    };
 
     promptOrder() {
         const userInput = prompt('What would you like to order? ');
         const orderSplit = userInput.split(' ');
-        this.order.push(orderSplit);
-        console.log(this.order);
+
+        this.order.push(orderSplit[0].toLowerCase(), orderSplit[1].split(','));
+        this.parseOrderType();
+        // console.log(this.order);
         // console.log(Breakfast[1]);
-    }
+    };
+
+    parseOrderType() {
+        const mealType = this.order[0];
+        // console.log(mealType, mealOptions);
+
+        if (mealType === 'breakfast') {
+            // this.orderBreakfast();
+            this.orderBreakfast();
+        } else if (mealType === 'lunch') {
+            this.orderLunch();
+        } else if (mealType === 'dinner') {
+            this.orderDinner();
+        }
+    };
+
+    parseOrderOptions() {
+        const mealType = this.order[0];
+        const mealChoices = this.order[1];
+        console.log('mealChoices', mealChoices)
+        if (!mealChoices.includes('1') && !mealChoices.includes('2')) {
+            console.log("Unable to process: Main is missing, side is missing");
+        } else if (!mealChoices.includes('1')) {
+            console.log("Unable to process: Main is missing");
+        } else if (!mealChoices.includes('2')) {
+            console.log("Unable to process: Side is missing");
+        }
+        if (!mealType.includes('dinner') && !mealChoices.includes('3')) {
+            const drink = "Water";
+            return drink;
+        }
+    };
+
+    orderBreakfast() {
+        const breakfastOrder = this.parseOrderOptions();
+        
+    };
+
+    orderLunch() {
+        this.parseOrderOptions();
+    };
+
+    orderDinner() {
+        this.parseOrderOptions();
+    };
 
 
 
